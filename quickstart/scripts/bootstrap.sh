8cffd7feacbb969dca773b240b49699e5895bbdc
Stephen Sachs <stesachs@amazon.com>
2020-04-03 19:02:34 +0200

    UNTESTED: Create the pcluster at Cloud9 bootstrap time

--- quickstart/scripts/bootstrap.sh
+++ quickstart/scripts/bootstrap.sh
@@ -3,8 +3,18 @@
 
 sudo pip-3.6 --disable-pip-version-check --no-cache-dir install aws-parallelcluster -U
 
-AWS_REGION=eu-west-1
-KEY_NAME=amzn
+export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | rev | cut -c 2- | rev)
+
+# Create and load ssh key
+aws ec2 create-key-pair \
+    --key-name pcluster_key \
+    --query KeyMaterial \
+    --output text > ~/.ssh/pcluster_key
+chmod 600 ~/.ssh/pcluster_key
+
+# Automatically add ssh key into agent. we need to make the agent stop asking for a password
+echo 'eval `ssh-agent`' >> ~/.bashrc
+echo 'ssh-add ~/.ssh/pcluster_key' >> ~/.bashrc
 
 mkdir -p ~/.parallelcluster
 cat >> ~/.parallelcluster/config <<EOF
@@ -14,16 +24,16 @@ update_check = true
 sanity_check = true
 
 [aws]
-aws_region_name = ${AWS_REGION}
+aws_region_name = ${AWS_DEFAULT_REGION}
 
 [aliases]
 ssh = ssh {CFN_USER}@{MASTER_IP} {ARGS}
 
 [cluster covid]
-key_name = ${KEY_NAME}
+key_name = pcluster_key
 base_os = ubuntu1804
 scheduler = slurm
-master_instance_type = c5n.18xlarge
+master_instance_type = c5n.9xlarge
 compute_instance_type = c5n.18xlarge
 vpc_settings = public-private
 fsx_settings = fsx
@@ -46,3 +56,8 @@ master_subnet_id = ${master_subnet_id}
 compute_subnet_id = ${compute_subnet_id}
 
 EOF
+
+
+# screen decouples the pcluster command so that the Cloud9 CFN template does not time out
+# pcluster is for some reason not ine th path.
+screen -d -m /usr/local/bin/pcluster create -t covid covid-cluster
